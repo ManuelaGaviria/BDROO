9ac84e3cbc922fdc710e755036e3c3c7eb8ba5b1
ManuelaGaviria <manugaviria0507@gmail.com>
2024-11-18 21:08:20 -0500

    Ajustes para el ejecutable

--- scriptEjecutable.sql
+++ scriptEjecutable.sql
@@ -1,4 +1,5 @@
 start C:\Users\ASUS\Documents\GitHub\BDROO\drop.sql;
+start C:\Users\ASUS\Documents\GitHub\BDROO\sequences.sql;
 start C:\Users\ASUS\Documents\GitHub\BDROO\createType.sql
 start C:\Users\ASUS\Documents\GitHub\BDROO\createTable.sql
 start C:\Users\ASUS\Documents\GitHub\BDROO\alterTable.sql
